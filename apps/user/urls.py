f970fa8a9e0317e48febda065a6bc1f9065c5c27
Sijal Manandhar <sijal.m06@gmail.com>
2024-08-02 17:54:16 +0545

    `logout_view` added, header styles changed

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -6,6 +6,7 @@ app_name = "user"
 
 urlpatterns = [
     path("", profile_view, name="profile"),
+    path("logout/", logout_view, name="logout"),
     path("login/", login_view, name="login"),
     path("register/", register_view, name="register"),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 
 def profile_view(request):
     return render(request, "user/profile.html")
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("user:login")
 
 
 def login_view(request):
